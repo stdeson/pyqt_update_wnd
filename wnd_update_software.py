31dfdb8f4404a37ec71b9ba2f812a2500976e85d
decenfrontier <decenfrontier@gmail.com>
2024-07-26 23:02:28 +0800

    fix

--- wnd_update_software.py
+++ wnd_update_software.py
@@ -1,19 +1,18 @@
 import os
-import shutil
-import webbrowser
 
+from memwin import XProcess
 from PySide2.QtCore import  QThread, Signal
 from PySide2.QtWidgets import QDialog, QMessageBox
 
 from .file_download_module import download_file
-from .utils import calculate_file_md5, copy_tree_safe, file_remove, remove_dir_safe
+from .utils import calculate_file_md5, file_remove
 from . import update_image_rc
 from .ui_winUpdate import Ui_Form
 
 
 class WndUpdateSoftware(QDialog, Ui_Form):
     sig_update_finish_restart = Signal()  # 更新完成重启
-    def __init__(self, parent=None, client_version="v0.1.0", patcher_save_path="./patcher.zip"):
+    def __init__(self, parent=None, client_version="v0.1.0"):
         super().__init__(parent)
         self.setupUi(self)
         self.setWindowTitle('软件更新')
@@ -41,7 +40,6 @@ class WndUpdateSoftware(QDialog, Ui_Form):
         self.textEdit.setText("正在检查更新...")
 
         self.client_version = client_version
-        self.patcher_save_path = patcher_save_path
         latest_version = "查询中..."
         self.label_2.setText(latest_version)
         self.label_bbh.setText(f'最新版本:{latest_version} 当前版本: {self.client_version}')
@@ -51,23 +49,8 @@ class WndUpdateSoftware(QDialog, Ui_Form):
         self.label_bbh.setText(f'最新版本:{latest_version} 当前版本: {self.client_version}')
         self.textEdit.setPlainText(data.get('update_info'))
         self.patcher_download_url = data.get('patcher_download_url')
+        self.installer_download_url = data.get('installer_download_url')
         self.md5 = data.get('md5')
-        self.force_update = data.get('force_update', False)
-        if self.force_update:
-            if self.patcher_download_url:  
-                # 如果提供了下载链接，说明是安装包的，要浏览器打开下载
-                self.label_2.setText("版本过低无法打补丁，请下载新的安装包直接覆盖安装一遍")
-                self.btn_azgx.hide()
-                self.btn_tgbb.hide()
-                self.btn_ok.show()
-                QMessageBox.information(
-                    self, '版本号过低无法打补丁', '请到浏览器下载新的安装包, 点击OK后自动前往下载页面')
-                webbrowser.open(self.patcher_download_url)
-                return
-            else:
-                # 如果没有提供下载链接，说明该版本已废弃
-                self.label_2.setText("该版本已废弃")
-            return
 
         if latest_version == self.client_version or latest_version == '':
             self.label_2.setText("你使用的是最新版本")
@@ -92,8 +75,7 @@ class WndUpdateSoftware(QDialog, Ui_Form):
         self.btn_tgbb.setEnabled(False)
 
         self.thd_download_file = ThdDownloadFile(
-            download_url=self.patcher_download_url,
-            save_path=self.patcher_save_path,
+            download_url=self.installer_download_url,
             wnd=self,
             edt=self.label_zt,
             process_bar=self.progressBar,
@@ -102,40 +84,21 @@ class WndUpdateSoftware(QDialog, Ui_Form):
         self.thd_download_file.start()
 
     def on_download_file_finish(self, download_result, save_path):
-        patcher_zip_path = save_path
+        installer_path = save_path
         if not download_result:
             self.label_zt.setText("下载更新失败")
-            file_remove(patcher_zip_path)
+            file_remove(installer_path)
             return
-        # 校验压缩包md5
-        download_md5 = calculate_file_md5(patcher_zip_path)
+        # 校验md5
+        download_md5 = calculate_file_md5(installer_path)
         print(f"计算出的md5: {download_md5}, 实际应该的md5: {self.md5}")
         if download_md5 != self.md5:
-            self.label_zt.setText("补丁包下载不完整, 请重新下载")
-            file_remove(patcher_zip_path)
+            self.label_zt.setText("安装包下载不完整, 请重新下载")
+            file_remove(installer_path)
             return
-        # 开始解压
-        extract_folder_path = "./patcher"
-        print("正在创建解压目录...")
-        # 如果存在的话先删除, 再创建新的patcher文件夹用于解压
-        if os.path.exists(extract_folder_path):
-            shutil.rmtree(extract_folder_path)
-        os.makedirs(extract_folder_path)
-        print(f"patcher_zip_path: {patcher_zip_path}, extract_folder_path:{extract_folder_path}")
-        # with zipfile.ZipFile(patcher_zip_path, 'r') as zf:
-        #     zf.extractall(extract_folder_path)
-        shutil.unpack_archive(patcher_zip_path, extract_folder_path)
-        print("解压完成")
-        # 替换biz目录下的内容
-        print('正在替换biz目录下的内容...')
-        source_dir = 'patcher/biz'
-        target_dir =  './biz'
-        if os.path.exists(source_dir):  # 如果目标目录存在才拷贝目录过去
-            # 复制资源文件
-            if copy_tree_safe(target_dir, source_dir):
-                # 成功则删除source_dir
-                remove_dir_safe(source_dir)
-        QMessageBox.information(self, "提示", "更新准备就绪, 请关闭软件后手动重启")
+        # 打开安装包
+        XProcess.create_process(installer_path)
+        QMessageBox.information(self, "提示", "更新准备就绪, 请关闭软件后直接安装")
 
 
 class ThdDownloadFile(QThread):
@@ -147,10 +110,10 @@ class ThdDownloadFile(QThread):
         super().__init__()
         self.wnd = kwargs.get('wnd')
         self.download_url = kwargs.get('download_url')
-        self.save_path = kwargs.get('save_path')
         self.edt = kwargs.get('edt')
         self.process_bar = kwargs.get('process_bar')
         self.sig_refresh_process_bar.connect(self.refresh_ui)
+        self.save_path = os.path.join(os.environ['USERPROFILE'], 'Downloads', 'qlinstaller.exe')
 
     def run(self):
         self.edt.setText('开始下载')
